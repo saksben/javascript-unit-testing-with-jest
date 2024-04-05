c01cc1865b3374e7d20e5d3ae628c0b242bebd93
Benjamin Saks <saksben2@gmail.com>
2024-04-05 00:51:18 -0700

    make async tests

--- tests/promotions/exchange.test.js
+++ tests/promotions/exchange.test.js
@@ -1,3 +1,22 @@
 const exchange = require("../../js/promotions/exchange/exchange");
 const exchangeRateProvider = require("../../js/promotions/exchange/exchangeRateProvider");
 
+describe("getExchangeRate", () => {
+  test("Returns correct exchange rate for US Dollars", (done) => {
+    jest
+      .spyOn(exchangeRateProvider, "callExchangeRateProvider")
+      .mockResolvedValue(1.21);
+
+    function callback(data) {
+      try {
+        expect(data.originalCurrency).toBe("GBP");
+        expect(data.newCurrency).toBe("USD");
+        expect(data.exchangeRate).toBeCloseTo(1.21);
+        done();
+      } catch (error) {
+        done(error);
+      }
+    }
+    exchange.getExchangeRate("USD", callback);
+  });
+});
--- tests/users/account.test.js
+++ tests/users/account.test.js
@@ -1,40 +1,94 @@
 const users = require("../../js/users/users");
 const account = require("../../js/users/account/account");
-const purchaseHistory = require("../../js/users/account/purchaseHistory/purchaseHistory")
-jest.unmock("../../js/users/account/account");
+const purchaseHistory = require("../../js/users/account/purchaseHistory/purchaseHistory");
+jest.mock("../../js/users/account/purchaseHistory/purchaseHistory");
 
 describe("getPastPurchases", () => {
-    test("Test gets past purchase history", () => {
-        const userId = 123;
-        const items = account.getPastPurchases(userId);
-        expect(items).toEqual([
-            {
-                name: "Punk Goes Pop - 90s",
-                tickets: 2,
-                price: 40.00,
-            },
-            {
-                name: "Adventures Live!",
-                tickets: 5,
-                price: 120.00,       
-            },
-            {
-                name: "Folk dance party!",
-                tickets: 3,
-                price: 75.00,
-            }
-        ]);
-    });
+  test("Test gets past purchase history", () => {
+    const userId = 123;
+    const items = account.getPastPurchases(userId);
+    expect(items).toEqual([
+      {
+        name: "Punk Goes Pop - 90s",
+        tickets: 2,
+        price: 40.0,
+      },
+      {
+        name: "Adventures Live!",
+        tickets: 5,
+        price: 120.0,
+      },
+      {
+        name: "Folk dance party!",
+        tickets: 3,
+        price: 75.0,
+      },
+    ]);
+  });
 
-    test("Throws error when readyState is not 4", () => {
-        jest.spyOn(purchaseHistory, 'getPurchaseHistory').mockReturnValue({
-            readyState: 2,        
-            onreadystatechange: null,
-            response: {
-                events: [
-                ],
-            }   
-        });
-        expect(() => account.getPastPurchases(123)).toThrow("Failed to get purchase history");
-    })
+  test("Throws error when readyState is not 4", () => {
+    jest.spyOn(purchaseHistory, "getPurchaseHistory").mockReturnValue({
+      readyState: 2,
+      onreadystatechange: null,
+      response: {
+        events: [],
+      },
+    });
+    expect(() => account.getPastPurchases(123)).toThrow(
+      "Failed to get purchase history"
+    );
+  });
+});
+
+// Use promises to test async code
+describe("createAccount", () => {
+  let newEmailAddress = "newuser2@pluralsight.com";
+  beforeEach(() => {
+    jest.spyOn(users, "userExists").mockResolvedValue(false);
+    jest.spyOn(users, "createUserId").mockReturnValue(2);
+  });
+
+  test("Returns user data when account created successfully", () => {
+    expect.hasAssertions();
+
+    return account.createAccount(newEmailAddress).then((userAccount) => {
+      expect(userAccount).toStrictEqual({
+        data: { userId: 2, username: newEmailAddress },
+      });
+    });
+  });
+
+  test("Returns user data when account created successfully - async/await", async () => {
+    expect.hasAssertions();
+
+    const userAccount = await account.createAccount(newEmailAddress);
+
+    expect(userAccount).toStrictEqual({
+      data: { userId: 2, username: newEmailAddress },
+    });
+  });
+
+  test("Returns user data when account created successfully - resolves", () => {
+    expect.hasAssertions();
+
+    expect(account.createAccount(newEmailAddress)).resolves.toStrictEqual({
+      data: { userId: 2, username: newEmailAddress },
+    });
+  });
+
+  test("Returns error message when user already exists", () => {
+    jest.spyOn(users, "userExists").mockReturnValue(true);
+    expect.hasAssertions();
+    return expect(account.createAccount(newEmailAddress)).rejects.toStrictEqual(
+      "User already exists"
+    );
+  });
+
+  test("Returns error message when user already exists - async/await", async () => {
+    jest.spyOn(users, "userExists").mockReturnValue(true);
+    expect.hasAssertions();
+    await expect(account.createAccount(newEmailAddress)).rejects.toStrictEqual(
+      "User already exists"
+    );
+  });
 });
