2f8ff77f605f1c550d61ec5674618f26b64827a5
Benjamin Saks <saksben2@gmail.com>
2024-04-04 22:00:26 -0700

    make first unit tests

--- tests/basket/basket.test.js
+++ tests/basket/basket.test.js
@@ -4,36 +4,56 @@ const { BasketItem } = require("../../js/basket/basketitem");
 let events = [];
 let items = [];
 
-test("Test calculates total basket price when no discount applied", () => {
+// beforeAll(() => {
+//   // Runs once before any tests have run
+// });
+
+// beforeEach(() => {
+//   // Runs before every test
+// });
+
+// afterEach(() => {
+//   // Runs after every test
+// });
+
+// afterAll(() => {
+//   // Runs after all tests have run
+// });
+
+// describe() groups related test classes
+describe("calculateTotal", () => {
+  let events = [];
+  let items = [];
+
+  beforeEach(() => {
     events = [
-        new Event(1, "A Night At The Proms", 2500.00, 2500, 2500),
-        new Event(2, "Taylor Swift", 50.00, 5500, 2500),
-        new Event(3, "Rage Against The Machine", 35.00, 2500, 2500),
+      new Event(1, "A Night At The Proms", 2500.0, 2500, 2500),
+      new Event(2, "Taylor Swift", 50.0, 5500, 2500),
+      new Event(3, "Rage Against The Machine", 35.0, 2500, 2500),
     ];
 
     items = [
-        new BasketItem(events[0], 1),
-        new BasketItem(events[1], 4),
-        new BasketItem(events[2], 2),
+      new BasketItem(events[0], 1),
+      new BasketItem(events[1], 4),
+      new BasketItem(events[2], 2),
     ];
+  });
+
+  test("Test calculates total basket price when no discount applied", () => {
     const total = basket.calculateTotal(items);
 
-    expect(total).toBeCloseTo(2770.00, 2)
-});
+    expect(total).toBeCloseTo(2770.0, 2);
+  });
 
-test("Test calculates total basket price with discount", () => {
-    events = [
-        new Event(1, "A Night At The Proms", 2500.00, 2500, 2500),
-        new Event(2, "Taylor Swift", 50.00, 5500, 2500),
-        new Event(3, "Rage Against The Machine", 35.00, 2500, 2500),
-    ];
-
-    items = [
-        new BasketItem(events[0], 1),
-        new BasketItem(events[1], 4),
-        new BasketItem(events[2], 2),
-    ];
+  test("Test calculates total basket price with discount", () => {
     const total = basket.calculateTotal(items, 800);
 
-    expect(total).toBeCloseTo(1970.00, 2)
+    expect(total).toBeCloseTo(1970.0, 2);
+  });
+});
+
+describe("showAdverts", () => {
+  test("Does not show adverts for premium users", () => {});
+
+  test("Show adverts for non-premium users", () => {});
 });
--- tests/events/event.test.js
+++ tests/events/event.test.js
@@ -4,5 +4,5 @@ test("Returns Sold Out tagline when no tickets left", () => {
   const event = new Event(1, "Summer BBQ", 40.0, 100, 0);
   const tagLine = getTagLine(event, 10, true)
 
-  expect(tagLine).toBe("Evemt Sold Out")
+  expect(tagLine).toBe("Event Sold Out!")
 });
